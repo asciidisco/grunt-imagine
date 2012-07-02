c303b9b981f8827f142645adab9355b0017c36f7
asciidisco <public@asciidisco.com>
2012-07-02 19:37:36 +0200

    Fixed imagine task itself & node version in package.json

--- tasks/imagine.js
+++ tasks/imagine.js
@@ -1,1 +1,19 @@
-imagine.js
+/*
+ * grunt-imagine
+ * https://github.com/asciidisco/grunt-imagine
+ *
+ * Copyright (c) 2012 "asciidisco" Sebastian Golasch
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+
+  // ==========================================================================
+  // TASKS
+  // ==========================================================================
+
+  grunt.registerTask('imagine', 'Aint doing nothing at the moment.', function() {
+    grunt.log.write('Please take a look at the Readme here if you want to know about how to use this: https://github.com/asciidisco/grunt-imagine/README.md');
+  });
+
+};
