8bc5ce82fc166be0878fa4c0f1d8754a2ee3d0b8
asciidisco <public@asciidisco.com>
2012-07-05 17:46:24 +0200

    Added some spriting stuff (very basic at the moment)

--- tasks/sprites.js
+++ tasks/sprites.js
@@ -15,61 +15,65 @@ module.exports = function(grunt) {
         imageFolder = 'img/icons36',
         processedImageFiles = [];
 
-grunt.registerTask('sprites', 'Generate sprite maps and css files from png images', function () {
+    grunt.registerMutliTask('sprites', 'Generate sprite maps and css files from png images', function () {
+        var done = this.done();
+        console.log(this.files);
+        done();
 
-fs.readdir(imageFolder, function (err, files) {
-    var fileslength = files.length;
-    files.forEach(function (file) {
-        if (path.extname(file) === '.png') {
-            fs.readFile(imageFolder + '/' + file, function (err, data) {
-                filesRead++;
-                allImages.push(data.toString('base64'));
-                processedImageFiles.push(imageFolder + '/' + file);
-                if (filesRead === (fileslength -1)) {
-                    runSpriteGenerator(allImages);
+
+        /*fs.readdir(imageFolder, function (err, files) {
+            var fileslength = files.length;
+            files.forEach(function (file) {
+                if (path.extname(file) === '.png') {
+                    fs.readFile(imageFolder + '/' + file, function (err, data) {
+                        filesRead++;
+                        allImages.push(data.toString('base64'));
+                        processedImageFiles.push(imageFolder + '/' + file);
+                        if (filesRead === (fileslength -1)) {
+                            runSpriteGenerator(allImages);
+                        }
+                    });
+                } else {
+                    fileslength--;
                 }
             });
-        } else {
-            fileslength--;
+        });
+
+        function generateCSSFile (imageData, images) {
+            var imageClasses = '';
+            var fileContents = '';
+            images.forEach(function (image, idx) {
+                if (idx > 0) {
+                    imageClasses += ', ';
+                }
+                imageClasses += '.' + image.replace('.png', '').replace(/\//g, '_');
+            });
+
+            fileContents += imageClasses + ' {' + '\n' + '    background: url("' + spriteFile + '") no-repeat;\n' + '}\n\n'; 
+            imageData.heights.forEach(function (height, idx) {
+                fileContents += '.' + images[idx].replace('.png', '').replace(/\//g, '_') + ' {\n' + '    background-position: 0 ' +  (height - imageData.maxheight) + 'px;\n' + '}\n\n'; 
+            });
+
+            return fileContents;
         }
-    });
-});
 
-function generateCSSFile (imageData, images) {
-    var imageClasses = '';
-    var fileContents = '';
-    images.forEach(function (image, idx) {
-        if (idx > 0) {
-            imageClasses += ', ';
-        }
-        imageClasses += '.' + image.replace('.png', '').replace(/\//g, '_');
-    });
+        function runSpriteGenerator (images) {
+            var ls = spawn('phantomjs', ['--web-security=no', 'sprite.js', JSON.stringify({images: images, spacing: spacing})]);
 
-    fileContents += imageClasses + ' {' + '\n' + '    background: url("' + spriteFile + '") no-repeat;\n' + '}\n\n'; 
-    imageData.heights.forEach(function (height, idx) {
-        fileContents += '.' + images[idx].replace('.png', '').replace(/\//g, '_') + ' {\n' + '    background-position: 0 ' +  (height - imageData.maxheight) + 'px;\n' + '}\n\n'; 
-    });
+            ls.stdout.on('data', function (data) {
+                externalData += data;
+                if (externalData.search('<<<<ENDIMAGE') !== -1) {
+                    ls.kill();
+                }
+            });
 
-    return fileContents;
-}
-
-function runSpriteGenerator (images) {
-    var ls = spawn('phantomjs', ['--web-security=no', 'sprite.js', JSON.stringify({images: images, spacing: spacing})]);
-
-    ls.stdout.on('data', function (data) {
-        externalData += data;
-        if (externalData.search('<<<<ENDIMAGE') !== -1) {
-            ls.kill();
-        }
-    });
-
-    ls.on('exit', function (code) {
-        var incomingData = JSON.parse(externalData.replace('<<<<ENDIMAGE', ''));
-        var dataBuffer = new Buffer(incomingData.image.replace(/^data:image\/png;base64,/, ''), 'base64');
-        fs.writeFile(spriteFile, dataBuffer, function(err) {});
-        fs.writeFile(cssFile, generateCSSFile(incomingData, processedImageFiles), function () {});
-    });
-}
+            ls.on('exit', function (code) {
+                var incomingData = JSON.parse(externalData.replace('<<<<ENDIMAGE', ''));
+                var dataBuffer = new Buffer(incomingData.image.replace(/^data:image\/png;base64,/, ''), 'base64');
+                fs.writeFile(spriteFile, dataBuffer, function(err) {});
+                fs.writeFile(cssFile, generateCSSFile(incomingData, processedImageFiles), function () {});
+            });
+        }*/
 
     });
 
