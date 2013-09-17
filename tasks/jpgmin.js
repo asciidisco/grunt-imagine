7b073dd4d3c7e71a0c791e374e19c9a489c84812
btamas <btamas@dev.brozie>
2013-09-17 13:08:59 +0200

    jpegmin need -d option to define output folder

--- tasks/jpgmin.js
+++ tasks/jpgmin.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
     var jpgTools = [{
             executable: 'jpegoptim',
             isAvailable: false,
-            flags: ['-f', '--strip-all', '<quality>', '<inputFile>', '<outputFolder>']
+            flags: ['-f', '--strip-all', '<quality>', '<inputFile>', '-d', '<outputFolder>']
         }, {
             executable: 'jpegtran',
             isAvailable: false,
